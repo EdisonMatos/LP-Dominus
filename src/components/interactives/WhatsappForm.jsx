8e1156107c7d27ea9f796c29828cda746aee247b
Gabriel de Souza da Silva <gabrielsouza65115@gmail.com>
2025-03-29 12:58:40 -0300

    fix: Formata mensagem do Whatsapp

--- src/components/interactives/WhatsappForm.jsx
+++ src/components/interactives/WhatsappForm.jsx
@@ -10,7 +10,7 @@ import { UserCheck } from "lucide-react";
 import { Phone } from "lucide-react";
 import { FolderPen } from "lucide-react";
 import { CiCreditCard1 } from "react-icons/ci";
-
+import content from "../../content/content";
 
 const WhatsappForm = () => {
   const [name, setName] = useState("");
@@ -169,6 +169,7 @@ const WhatsappForm = () => {
       return formattedPhone.replace(/^(\d{2})(\d{5})(\d{4})$/, "($1) $2-$3");
     };
     const whatsappMessage = `Olá! 
+Vim através do site, e essa mensagem se refere a uma inscrição no curso ${content.texts.contactForm.title}.
 Meu nome é ${name}.
 Cpf: ${cpf}.
 Telefone: ${phone}.
