0ee9902a5f50629a464b59e9595018508401ee3c
Gabriel de Souza da Silva <gabrielsouza65115@gmail.com>
2025-06-30 14:51:25 -0300

    fix: Adiciona vídeo

--- src/components/sections/Structure.jsx
+++ src/components/sections/Structure.jsx
@@ -34,6 +34,18 @@ export default function Structure() {
             allowfullscreen
             className="rounded-md desktop1:max-w-[50%]"
           ></iframe>
+
+          <iframe
+            width="100%"
+            height="315"
+            src="https://www.youtube.com/embed/4yK79ZwffGI?si=cYLyENMgOy1t5fwG"
+            title="YouTube video player"
+            frameborder="0"
+            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+            referrerpolicy="strict-origin-when-cross-origin"
+            allowfullscreen
+            className="rounded-md desktop1:max-w-[50%] mt-8"
+          ></iframe>
         </SectionWrapper>
       </SectionArea>
     </div>
