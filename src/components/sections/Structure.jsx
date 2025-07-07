72487ae592a2cfb4ed0b67488ca84a5b5d49f81c
Gabriel de Souza da Silva <gabrielsouza65115@gmail.com>
2025-07-07 20:49:22 -0300

    fix: Video novo

--- src/components/sections/Structure.jsx
+++ src/components/sections/Structure.jsx
@@ -28,7 +28,7 @@ export default function Structure() {
           <iframe
             width="100%"
             height="315"
-            src="https://youtube.com/shorts/mPrQCQBGXhs"
+            src="https://www.youtube.com/embed/mPrQCQBGXhs"
             frameborder="0"
             allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
             allowfullscreen
