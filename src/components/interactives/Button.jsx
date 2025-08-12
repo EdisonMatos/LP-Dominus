66595ad8b48953c72a80874521fb67f630dd5c16
Gabriel de Souza da Silva <gabrielsouza65115@gmail.com>
2025-08-12 10:07:16 -0300

    fix: Button modal About

--- src/components/interactives/Button.jsx
+++ src/components/interactives/Button.jsx
@@ -80,7 +80,7 @@ export default function Button({
         <div className="w-auto">
           <button
             onClick={onClick}
-            className={`flex ${className} ${sizeFeatures} shadow-custom-opacityButton shadow-shadowHero/20 bg-buttonColor flex-row items-center justify-around transition ${color} text-labelButtons desktop1:hover:scale-110`}
+            className={`flex ${className} ${sizeFeatures} shadow-custom-opacityButton shadow-shadowHero/20 bg-primary flex-row items-center justify-around transition ${color} text-black desktop1:hover:scale-110`}
           >
             <div
               className={`flex items-center text-center ${gap} min-h-[24px]`}
